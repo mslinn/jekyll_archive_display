67f83c18cb9eb95a086b8f450ab8d010d03e4c72
Mike Slinn <mslinn@micronauticsresearch.com>
2022-03-24 02:23:33 -0400

    -

--- lib/archive_display.rb
+++ lib/archive_display.rb
@@ -30,7 +30,7 @@ module Jekyll
       source = context.registers[:site].config["source"]
       tar_name = "#{source}/#{@archive_name}"
       @logger.debug "tar_name=#{tar_name}"
-      traverse_tar(tar_name).join("\n")
+      traverse_tar(tar_name)
     end
 
     private
@@ -76,6 +76,7 @@ module Jekyll
                    .compact
                    .sort_by { |entry| entry[:name] }
                    .map { |entry| display_entry entry }
+                   .join("\n")
           @logger.debug { "traverse_tar: result = #{result}" }
           result
         end
