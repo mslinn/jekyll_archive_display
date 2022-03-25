58c20bd3862259ffd91e41afcf1b92a32d9f8d1b
Mike Slinn <mslinn@micronauticsresearch.com>
2022-03-25 11:39:28 -0400

    -

--- lib/archive_display.rb
+++ lib/archive_display.rb
@@ -19,7 +19,7 @@ module Jekyll
     # @return [void]
     def initialize(tag_name, archive_name, tokens)
       super(tag_name, archive_name, tokens)
-      @logger = PluginMetaLogger.new_logger(self)
+      @logger = PluginMetaLogger.instance.new_logger(self)
       @archive_name = archive_name.strip
     end
 
