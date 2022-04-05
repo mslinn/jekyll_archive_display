d8ad4cc66d26f001b78a75660538f06f33b7610e
Mike Slinn <mslinn@micronauticsresearch.com>
2022-04-05 11:28:48 -0400

    -

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,7 @@ require "jekyll_plugin_logger"
 require_relative "../lib/archive_display"
 
 RSpec.configure do |config|
-  config.run_all_when_everything_filtered = true
   config.filter_run :focus
   config.order = "random"
+  config.run_all_when_everything_filtered = true
 end
